e6a727e36c950178cbee656e387334dee1241d51
Zohar Suslovich <zohar.suslovich@shu.edu>
2018-01-31 20:18:50 -0500

    First Assignment

--- src/Student.java
+++ src/Student.java
@@ -1,37 +1,74 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Student {
-	private String firstName;
-	private String lastName;
-	private int idNumber;
-	//TEST
-	public Student(String firstName, String lastName, int idNumber) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.idNumber = idNumber;
-	}
-	
-	public String getFirstName() {
-		return this.firstName;
-	}
-	
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	
-	public String getLastName() {
-		return this.lastName;
-	}
-	
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	public int getIdNumber() {
-		return this.idNumber;
-	}
-	
-	public void setIdNumber(int idNumber) {
-		this.idNumber = idNumber;
-	}	
-	
+    private String firstName;
+    private String lastName;
+    private int idNumber;
+    private String email;
+    private ArrayList<Course> courses;
+
+    public Student(String firstName, String lastName, int idNumber, String email, ArrayList<Course> courses) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.email = email;
+        this.courses = courses;
+    }
+
+    public Student(){
+        this.courses = new ArrayList<>();
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getIdNumber() {
+        return this.idNumber;
+    }
+
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<Course> courses) {
+        this.courses = courses;
+    }
+
+    public void addCourse(Course course){
+        if (!this.courses.contains(course))
+            this.courses.add(course);
+        else
+            System.out.println("This Student is already enrolled in this course");
+    }
+
+    public void removeCourse(Course course){
+        this.courses.remove(course);
+    }
+
 }
--- src/StudentView.java
+++ src/StudentView.java
@@ -1,29 +1,40 @@
+import java.util.IllformedLocaleException;
 
 public class StudentView {
-	private Student student;
-	
-	public StudentView(Student student) {
-		this.student = student;
-	}
-	
-	public StudentView() {
-		
-	}
-	
-	public void printStudentDetails(Student student) {
-		this.student = student;
-		printStudentDetails();
-	}
-	
-	public void setStudentDetails(Student student) {
-	
-	}
-	
-	public void printStudentDetails() {
-		if (this.student == null) throw new IllegalArgumentException("student");		
+    private Student student;
 
-		System.out.println("Student's first name is " + this.student.getFirstName());
-		System.out.println("Student's last name is " + this.student.getLastName());
-		System.out.println("Student's id number is " + this.student.getIdNumber());
-	}
+
+    public StudentView(Student student) {
+        this.student = student;
+    }
+
+    public StudentView(){
+
+    }
+    public void printStudentDetails(Student student) {
+        this.student = student;
+        printStudentDetails();
+    }
+
+    public void setStudentDetails(Student student) {
+
+    }
+
+    public void printStudentDetails() {
+        if (this.student == null) throw new IllegalArgumentException("student");
+        if (this.student.getCourses().size() < 2) {
+            System.out.println("Student must be registered in 2 or more courses");
+        }
+        else {
+            System.out.println("Student's first name is " + this.student.getFirstName());
+            System.out.println("Student's last name is " + this.student.getLastName());
+            System.out.println("Student's id number is " + this.student.getIdNumber());
+            System.out.println("Student's email address is " + this.student.getEmail());
+            System.out.println("Student's list of courses is: ");
+            for (Course course : this.student.getCourses()) {
+                System.out.println(course.getCourseName() + " Taught by " + course.getProfessor().getFirstName() + " " + course.getProfessor().getLastName());
+            }
+            System.out.println();
+        }
+    }
 }
